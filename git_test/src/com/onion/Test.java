d05e728145e17a9c61eca3892404571bd3651c70
onion <1493393635@qq.com>
2020-11-17 08:37:57 +0800

    把本地工程提交到本地仓库

---
+++ git_test/src/com/onion/Test.java
@@ -0,0 +1,18 @@
+package com.onion;
+
+/**
+ * @author onion
+ * @create 2020-11-17-8:25
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
